db38a7327b9c4e94cc9316163e538b90d2960235
Robert Var <mail.robertvari@gmail.com>
2022-10-30 15:51:41 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>My React Website</h1>
+      <Footer/>
     </div>
   );
 }
