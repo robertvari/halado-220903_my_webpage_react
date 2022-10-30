4c6b1bb93ad24099ad111f7bb7d664d75e0f6742
Robert Var <mail.robertvari@gmail.com>
2022-10-30 18:32:19 +0100

    update

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
+
+function ServiceCard({title, description, photo}){
+  return(
+    <section className="service-card">
+      <img src={photo} alt={photo} />
+      
+      <section className="card-content">
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </section>
+    </section>
+  )
+}
+
+
 export default function Services() {
   return (
-    <div className='content-container'>Services</div>
+    <div className='content-container'>
+      <ServiceCard photo="https://source.unsplash.com/random/?wedding&1" title="Esküvői fotózás"  description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales vehicula dui, sit amet tincidunt sapien mollis ut. Sed enim leo, blandit id tempus eget, lacinia quis turpis. "/>
+      <ServiceCard title="Természetfotók" photo="https://source.unsplash.com/random/?nature&1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales vehicula dui, sit amet tincidunt sapien mollis ut. Sed enim leo, blandit id tempus eget, lacinia quis turpis. "/>
+      <ServiceCard title="Rendezvények szervezése" photo="https://source.unsplash.com/random/?events&1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales vehicula dui, sit amet tincidunt sapien mollis ut. Sed enim leo, blandit id tempus eget, lacinia quis turpis. "/>
+      <ServiceCard title="Rendezvények szervezése" photo="https://source.unsplash.com/random/?events&1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales vehicula dui, sit amet tincidunt sapien mollis ut. Sed enim leo, blandit id tempus eget, lacinia quis turpis. "/>
+      <ServiceCard title="Rendezvények szervezése" photo="https://source.unsplash.com/random/?events&1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales vehicula dui, sit amet tincidunt sapien mollis ut. Sed enim leo, blandit id tempus eget, lacinia quis turpis. "/>
+      <ServiceCard title="Rendezvények szervezése" photo="https://source.unsplash.com/random/?events&1" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales vehicula dui, sit amet tincidunt sapien mollis ut. Sed enim leo, blandit id tempus eget, lacinia quis turpis. "/>
+    </div>
   )
 }
