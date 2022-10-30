1435a5cfc2342f4f0ed862c0848c96a54b79a890
Robert Var <mail.robertvari@gmail.com>
2022-10-30 18:39:13 +0100

    About page

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div className='content-container'>About</div>
+    <div className='content-container about-container'>
+      <img src="https://source.unsplash.com/random/?portrait" alt="" />
+      <section>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid recusandae, consequatur aperiam qui earum praesentium eius quisquam magnam perferendis optio enim reprehenderit, hic sapiente nesciunt beatae tenetur molestiae ipsam necessitatibus! Commodi iste ipsa, repellat sit et sunt aspernatur odit illo quidem minima tempora at nostrum, placeat, a nesciunt. Eius laudantium eligendi sapiente delectus vel, eaque distinctio id eum alias perspiciatis. Itaque nihil eveniet provident voluptate, officia enim pariatur suscipit, repellat, tempore ipsa nostrum ex illum eligendi odit rem incidunt consequuntur. Iure voluptates libero dignissimos quo esse eaque voluptatem quisquam exercitationem laboriosam saepe magnam, ipsa aliquid ab expedita, consectetur, officiis maxime?</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga quibusdam qui quia nemo magni unde suscipit error possimus voluptatum ratione nostrum impedit autem facere porro voluptatem, quasi odit quas repellendus distinctio eius voluptas cupiditate aspernatur placeat. Mollitia porro laboriosam enim natus eos placeat necessitatibus dolor magnam, adipisci omnis nulla earum!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, tempore, aliquid obcaecati ipsum expedita in laborum nesciunt adipisci culpa dolore modi animi repellat suscipit quam unde? Animi iusto illo nobis, dolore quaerat enim vitae voluptates minus laborum voluptas incidunt, eligendi unde aperiam, qui beatae. Vitae dolore quis porro dolorem! Aperiam odio corrupti consequatur architecto ex hic, veniam explicabo similique rem neque ratione unde deserunt, illo fugit? Explicabo nisi maxime magnam. Officia eaque deserunt repellendus ratione iure ut consequatur voluptatum nisi delectus, praesentium reprehenderit quos dolorem optio itaque. Omnis quas vitae qui modi laborum odio exercitationem cum id nulla fuga maiores nisi doloribus pariatur rerum harum voluptates quos illum ut, dignissimos libero adipisci. Officiis voluptatum vel nulla iste, itaque sapiente omnis expedita inventore autem laudantium dignissimos sunt odio dolores illo in eius dolor maxime obcaecati consequatur quod nemo est. Doloribus perferendis, optio quam distinctio accusamus placeat dignissimos officiis sit ullam labore adipisci, excepturi veniam repudiandae possimus perspiciatis incidunt ducimus recusandae nihil!</p>
+      </section>
+    </div>
   )
 }
