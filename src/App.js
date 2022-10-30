1bd6929375bce803e359685cf3f64d10f7d55d8b
Robert Var <mail.robertvari@gmail.com>
2022-10-30 17:32:03 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import './css/App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Services from "./components/Services"
+import About from "./components/About"
+import Contact from "./components/Contact"
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <BrowserRouter>
+      <div className="App">
+        <Header/>
 
-        <Home/>
+          <Routes>
+            <Route path="/contact" element={<Contact/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/services" element={<Services/>}/>
+            <Route path="/" element={<Home/>}/>
+          </Routes>
 
-      <Footer/>
-    </div>
+        <Footer/>
+      </div>
+    </BrowserRouter>
   );
 }
 
