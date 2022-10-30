8d3f0f2c890655683cc50832b1886acc8aa0dd39
Robert Var <mail.robertvari@gmail.com>
2022-10-30 18:54:28 +0100

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div className='content-container'>Contact</div>
+    <div className='content-container contact-container'>
+      <h1>Kérdésed van?</h1>
+      <p>Küldj egy üzenetet az alábbi űrlap segítségével.</p>
+      
+      <form action="">
+        <input type="text" placeholder='Név'/>
+        <input type="email" placeholder='Email'/>
+        <textarea name="" id="" cols="30" rows="10"></textarea>
+        <button>Küldés</button>
+      </form>
+    </div>
   )
 }
